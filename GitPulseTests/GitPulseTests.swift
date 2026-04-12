20a97d0dc228dcec36ca5416be1a4d72a6f33711
Anthony Grimaldi <ajgrimaldi1997@gmail.com>
2026-04-12 09:24:12 -0400

    Initial Commit

---
+++ GitPulseTests/GitPulseTests.swift
@@ -0,0 +1,19 @@
+//
+//  GitPulseTests.swift
+//  GitPulseTests
+//
+//  Created by Anthony Grimaldi on 4/12/26.
+//
+
+import Testing
+@testable import GitPulse
+
+struct GitPulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
